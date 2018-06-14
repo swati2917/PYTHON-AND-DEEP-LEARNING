86c2909a5ecffdf24aa5a97f542f75ef5631fb7a
SWATI SINGH <singhswati.ri@gmail.com>
2018-06-14 11:29:04 -0500

    Add files via upload

---
+++ icp1/number division.py
@@ -0,0 +1,7 @@
+first=int(input("Enter First Number:"))
+second=int(input("Enter Second Number:"))
+
+quotient=first//second
+remainder=first%second
+
+print("Quotient is" ,quotient , "and remainder is", remainder)
